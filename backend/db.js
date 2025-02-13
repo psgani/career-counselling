3810bfc7eb0d60865e8c8e68188781e27bde5263
psgani <131879872+psgani@users.noreply.github.com>
2025-02-13 15:27:34 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = "mongodb://127.0.0.1";
+const url = "mongodb+srv://saiganeshpunnam:PSGcbit123@cccluser.feysu.mongodb.net/?retryWrites=true&w=majority&appName=CCCluser";
 
 const connectToMongo = async()=>{
     await mongoose.connect(url);
